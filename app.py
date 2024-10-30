fea8fe24d4e2e434a7b6610543a89c1a83aa174f
Терёхин Даниил <terekhin.daniil.a@gmail.com>
2024-10-30 17:16:23 +0100

    commented app.run for testing

--- app.py
+++ app.py
@@ -387,5 +387,5 @@ def form_trigger_status():
 ############################################################
 ############################################################
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(debug=True)
